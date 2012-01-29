b1ba9bc0659d319d584a1b1fddb7ae855c239ac2
Marc Baechinger <marc.baechinger@gmail.com>
2012-01-29 20:38:49 +0100

    remove absolete winston dependency

--- routes/fs-management.js
+++ routes/fs-management.js
@@ -1,32 +1,33 @@
-var fs = require("fs"),
-	logger = require("winston"),
-	util = require("../app/util.js"),
-	jsFileMatcher = /\.js$/,
-	lessFileMatcher = /\.(less|css)$/,
-	createSourceTree = function(basePath, path) {
-		var fsNode = {
-				path: path,
-				realPath: basePath + "/" + path,
-				name: path.substring(path.lastIndexOf("/") + 1),
-				children: []
-			},
-			tree = fs.readdirSync(basePath + "/" + path);
+/*global require:false, exports: false */
+(function () {
+	"use strict";
+	var fs = require("fs"),
+		jsFileMatcher = /\.js$/,
+		lessFileMatcher = /\.(less|css)$/,
+		createSourceTree = function (basePath, path) {
+			var fsNode = {
+					path: path,
+					realPath: basePath + "/" + path,
+					name: path.substring(path.lastIndexOf("/") + 1),
+					children: []
+				},
+				tree = fs.readdirSync(basePath + "/" + path);
 			
-		tree.forEach(function(filename, idx) {
-			var dirPath = path + "/" + filename,
-				dirStat = fs.statSync(basePath + "/" + dirPath);
-			if (dirStat.isDirectory()) {
-				fsNode.children.push(createSourceTree(basePath, dirPath));
-			} else if (filename.match(jsFileMatcher) /*|| this.match(lessFileMatcher)*/){
-				fsNode.children.push({
-					path: path + "/" + filename.toString(),
-					name: filename.toString(),
-					lintable: filename.match(jsFileMatcher),
-					lessable: filename.match(lessFileMatcher)
-				});
-			}
-		});
-		return fsNode;
-	};
-
-exports.createSourceTree = createSourceTree;
+			tree.forEach(function (filename) {
+				var dirPath = path + "/" + filename,
+					dirStat = fs.statSync(basePath + "/" + dirPath);
+				if (dirStat.isDirectory()) {
+					fsNode.children.push(createSourceTree(basePath, dirPath));
+				} else if (filename.match(jsFileMatcher)) {
+					fsNode.children.push({
+						path: path + "/" + filename.toString(),
+						name: filename.toString(),
+						lintable: filename.match(jsFileMatcher),
+						lessable: filename.match(lessFileMatcher)
+					});
+				}
+			});
+			return fsNode;
+		};
+	exports.createSourceTree = createSourceTree;
+}());
