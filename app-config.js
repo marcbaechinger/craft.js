5298366a2c4df650904c0fb2e32aba589c1c0355
Marc Baechinger <marc.baechinger@gmail.com>
2012-07-27 22:58:30 +0200

    configuration in a pure json file

--- app-config.js
+++ app-config.js
@@ -1,20 +1,23 @@
 /*jslint node: true */
 (function () {	
+	var fs = require("fs"),
+		config = JSON.parse(fs.readFileSync('app-config.json'));
+	
 	exports.server = {
-		port: 3000
+		port: config.server.port
 	};
 	exports.context = {
-		src: "repo",
-		dist: "dist",
-		jobs: "jobs",
-		default: "repo"
+		src: config.context.src,
+		dist: config.context.dist,
+		jobs: config.context.jobs,
+		default: config.context.default
 	};
 	exports.path = {
 		base: __dirname,
-		src: __dirname + "/resources",
-		dist: __dirname + "/dist",
-		jobs: __dirname + "/jobs",
-		views: __dirname + "/views",
-		docroot: __dirname + '/public'
-	};	
+		src: __dirname + "/" + config.path.src,
+		dist: __dirname + "/" + config.path.dist,
+		jobs: __dirname + "/" + config.path.jobs,
+		views: __dirname + "/" + config.path.views,
+		docroot: __dirname + "/" + config.path.docroot
+	};
 }());
